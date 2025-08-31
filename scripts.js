0205588df6605658371354f96a91b8a88114f375
Leati Perise <Perisi02@myunitec.ac.nz>
2025-09-01 07:30:09 +1200

    Commiting while everything still works.

---
+++ scripts.js
@@ -0,0 +1,57 @@
+const formPage1 = document.getElementById("form-page1");
+const formPage2 = document.getElementById("form-page2");
+const formPage3 = document.getElementById("form-page3");
+const formPage4 = document.getElementById("form-page4");
+const formPage5 = document.getElementById("form-page5");
+
+const progressBar = document.querySelector("progress")
+
+function showPage(pageNumber) {
+    switch (pageNumber) {
+        case 1:
+            formPage1.style.display = 'block';
+            formPage2.style.display = 'none';
+            formPage3.style.display = 'none';
+            formPage4.style.display = 'none';
+            formPage5.style.display = 'none';
+            progressBar.value= 20;
+            break;
+        case 2:
+            formPage1.style.display = 'none';
+            formPage2.style.display = 'block';
+            formPage3.style.display = 'none';
+            formPage4.style.display = 'none';
+            formPage5.style.display = 'none';
+            progressBar.value= 40;
+            break;
+        case 3:
+            formPage1.style.display = 'none';
+            formPage2.style.display = 'none';
+            formPage3.style.display = 'block';
+            formPage4.style.display = 'none';
+            formPage5.style.display = 'none';
+            progressBar.value= 60;
+            break;
+        case 4:
+            formPage1.style.display = 'none';
+            formPage2.style.display = 'none';
+            formPage3.style.display = 'none';
+            formPage4.style.display = 'block';
+            formPage5.style.display = 'none';
+            progressBar.value= 80;
+            break;
+        case 5:
+            formPage1.style.display = 'none';
+            formPage2.style.display = 'none';
+            formPage3.style.display = 'none';
+            formPage4.style.display = 'none';
+            formPage5.style.display = 'block';
+            progressBar.value=100;
+            break;
+        
+    }
+
+const bannerScene1 = document.getElementsByClassName(bannerScene1);
+
+bannerScene1.classList.add();
+}
